d9d3e0f9e696a3a2b3537a93ab4452d77fd6f3f4
HIDAN <btc.flexxx.btc@gmail.com>
2023-10-29 23:47:13 +0200

    ✨ Edit settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,29 +1,20 @@
 import pathlib
-from typing import Final, TypeAlias
+from typing import Final
 
 ROOT_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parents[1]
 ADDRESSES_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('data').joinpath('addresses.txt')
 ERC20_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('data').joinpath('erc20.json')
 
-
-
-# T_WEB3_CLIENT: TypeAlias = classmethod
-
-
-
-
-
 # Example
-
-OUTPUT_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('results')
-INPUT_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('app/input_urls.txt')
-
-T_URL: TypeAlias = str
-T_URLS: TypeAlias = list[T_URL]
-
-T_HTML_TEXT: TypeAlias = str
-T_HTML_TEXTS: TypeAlias = list[T_HTML_TEXT]
-
-T_FILE_NAME: TypeAlias = str
-T_TXT_FILE: TypeAlias = str
-T_JSON_FILE: TypeAlias = str
+# OUTPUT_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('results')
+# INPUT_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('app/input_urls.txt')
+#
+# T_URL: TypeAlias = str
+# T_URLS: TypeAlias = list[T_URL]
+#
+# T_HTML_TEXT: TypeAlias = str
+# T_HTML_TEXTS: TypeAlias = list[T_HTML_TEXT]
+#
+# T_FILE_NAME: TypeAlias = str
+# T_TXT_FILE: TypeAlias = str
+# T_JSON_FILE: TypeAlias = str
