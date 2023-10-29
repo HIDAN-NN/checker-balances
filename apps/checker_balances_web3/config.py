9528c95d7af4de82f74f4b0456e2501d88752020
HIDAN <btc.flexxx.btc@gmail.com>
2023-10-29 23:42:33 +0200

    🧰 Add config

---
+++ apps/checker_balances_web3/config.py
@@ -0,0 +1,74 @@
+networks_data = [
+    {
+        'chain': 'Ethereum',
+        'coin': 'ETH',
+        'chain_id': 1,
+        'rpc': 'https://rpc.ankr.com/eth',
+        'tokens': {
+            'USDT': '',
+            'USDC': '',
+        }
+    },
+    {
+        'chain': 'Arbitrum One',
+        'coin': 'ETH',
+        'chain_id': 42161,
+        'rpc': 'https://arb1.arbitrum.io/rpc',
+        'tokens': {
+            'USDT': '',
+            'USDC': '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+            'STG': '0x6694340fc020c5e6b96567843da2df01b2ce1eb6',
+        },
+    },
+    {
+        'chain': 'Optimism',
+        'coin': 'ETH',
+        'chain_id': 10,
+        'rpc': 'https://optimism.llamarpc.com',
+        'tokens': {
+            'USDT': '',
+            'USDC': '',
+        }
+    },
+    {
+        'chain': 'Avalanche',
+        'coin': 'AVAX',
+        'chain_id': 43114,
+        'rpc': 'https://api.avax.network/ext/bc/C/rpc',
+        'tokens': {
+            'USDT': '',
+            'USDC': '',
+        }
+    },
+    {
+        'chain': 'BSC',
+        'coin': 'BNB',
+        'chain_id': 56,
+        'rpc': 'https://bsc-dataseed.binance.org',
+        'tokens': {
+            'USDT': '',
+            'USDC': '',
+        }
+    },
+    {
+        'chain': 'Polygon',
+        'coin': 'MATIC',
+        'chain_id': 137,
+        'rpc': 'https://polygon-rpc.com',
+        'tokens': {
+            'USDC': '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+            'WMATIC': '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+            'AAVE': '0xD6DF932A45C0f255f85145f286eA0b292B21C90B',
+        }
+    },
+    {
+        'chain': 'Fantom',
+        'coin': 'FTM',
+        'chain_id': 250,
+        'rpc': 'https://rpc.ankr.com/fantom',
+        'tokens': {
+            'USDT': '',
+            'USDC': '',
+        }
+    },
+]
