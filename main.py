6e6a2962db52a17eaf726eea4324d8b38052e4c0
HIDAN <btc.flexxx.btc@gmail.com>
2023-11-06 11:38:21 +0200

    ✨ Edited logging

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    init_logging()
+    init_logging(is_verbose=False)
     main()
--- tools/init_logging.py
+++ tools/init_logging.py
@@ -1,14 +1,31 @@
 import logging
 import sys
 
+from colorlog import ColoredFormatter
+
 
 def init_logging(is_verbose: bool = False):
-    logging.basicConfig(
-        stream=sys.stdout,
-        format="[%(asctime)s.%(msecs)03d] "
-               "[PROCESS %(process)d %(processName)s] "
-               "[%(threadName)-10s] "
-               "%(levelname)s - %(message)s",
+    formatter = ColoredFormatter(
+        "%(log_color)s[%(asctime)s.%(msecs)03d] "
+        "[PROCESS %(process)d %(processName)s] "
+        "[%(threadName)-10s] "
+        "%(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.DEBUG if is_verbose else logging.INFO,
+        log_colors={
+            'DEBUG': 'cyan',
+            # 'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        secondary_log_colors={},
+        style='%'
     )
+
+    console = logging.StreamHandler(sys.stdout)
+    console.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(console)
+
+    root_logger.setLevel(logging.DEBUG if is_verbose else logging.INFO)
