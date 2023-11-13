98cb39a4f3a4ed37c7edabd7e5b336632ac62e2b
HIDAN <btc.flexxx.btc@gmail.com>
2023-11-13 12:58:24 +0200

    ✨ Edited main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main() -> None:
     asyncio.run(
         CheckerBalancesWeb3(
             check_native_balance_bool=True,
-            check_token_balance_bool=False,
+            check_token_balance_bool=True,
             show_empty_native_balances_bool=False,
             show_empty_token_balances_bool=False
         ).main()
